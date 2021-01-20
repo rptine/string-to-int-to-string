7250f20c1404679fc63c923a9a3d3fc4a7f028f5
Ryan Tine <ryanptine@gmail.com>
2021-01-20 16:39:52 -0600

    Variable renames. Add docstring. Refactor for clarity

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # string-to-int-to-string
-Systematic way of converting any string to an int and converting any int to a string. 
+Deterministic method of converting any string to an int, and also converting any int to a string. 
 
 ## Description
-Function ```stringToInt(s)``` converts any string to an integer (of base 10). Function ```intToString(i)``` converts an integer to a string.  <br/>
-These two functions will always produce identical outputs for the same input.
+Function ```stringToInt(s)``` converts any string to an integer. Conversely, function ```intToString(i)``` converts an integer to a string.  <br/>
+These two functions are both deterministic, and will always produce identical outputs for the same input.
 Thus, we can convert a string,```s```, to an int with the call ```stringToInt(s)```.  We could then convert this back to the original string, ```s```, with a call to ```intToString()```.  Thus, ```s = intToString(stringToInt(s))```.
--- stringToInt.py
+++ stringToInt.py
@@ -1,42 +1,87 @@
-def stringToBitList(message):
-    """Converts a string with alphabetic, numeric and special characters to a list of 0's and 1's"""
-    total = []
-    for character in message:
-        # Store Unicode code point of the character in c
-        c = ord(character) 
-        indiv = []
-        # Convert the value in c to binary, and store this binary as a list of 0's and 1's
-        while c > 0:
-            indiv = [(c % 2)] + indiv
-            c = c / 2
+import math
+
+def string_to_bit_list(string_to_convert):
+    """
+    Converts any string to a list of 0's and 1's, deterministaclly. Obtains the unicode value 
+    for each character in the string, converts each unicode value to a binary value represented
+    as a list of 0's and 1's, pads the binary reprsentation to 8 bits, and concatenates the list
+    of binaries for each character together.
+
+    Args:
+        string_to_convert: any string (no limit to character types of size)
+
+    Returns:
+        The input string represented as a list of 0's and 1's
+    """
+    total_uni_val_bit_list = []
+    for char in string_to_convert:
+        char_uni_val = ord(char) 
+        char_uni_val_bit_list = []
+        while char_uni_val >=1:
+            char_uni_val_bit_list = [char_uni_val % 2] + char_uni_val_bit_list
+            char_uni_val = math.floor(char_uni_val / 2)
         # Pad the binary representation to 8 bits
-        indiv = padBits(indiv,8)
-        # Sum the padded binary representations of each character 
-        total = total + indiv
-    return total
+        char_uni_val_bit_list = pad_bits(char_uni_val_bit_list,8)
+        total_uni_val_bit_list += char_uni_val_bit_list
+    return total_uni_val_bit_list
 
-def bitListToInt(bitList):
-    """Converts a list of 0's and 1's, to an int of base 2"""
-    return int(''.join([('0','1')[b] for b in bitList]),2)
 
-def stringToInt(message):
-    """Wrapper function for above two funtions to convert a 
-    string to a base 10 integer based on the ASCII values of
-     its characters"""
-    return bitListToInt(stringToBitList(message))
+def bit_list_to_int(bit_list):
+    """
+    Converts binary number represented as a list of 0's and 1's into its corresponding base 10
+    integer value.
 
-def binstringToBitList(binstring): 
+    Args:
+        bit_list: a binary number represented as a list of 0's and 1's
+
+    Returns:
+        The base 10 integer value of the input binary number
+    """
+    bit_string = ''.join([('0','1')[b] for b in bit_list])
+    return int(bit_string, 2)
+
+
+def string_to_int(string_to_convert):
+    """
+    Orchestrator function to convert a string to a base 10 integer, using the unicode values of the
+    characters in the string.
+
+    Args:
+        string_to_convert: any string
+
+    Returns:
+        A base 10 integer calculated from the unicode values of the characters in the string.
+    """
+    bit_list = string_to_bit_list(string_to_convert)
+    return bit_list_to_int(bit_list)
+
+
+def binstring_to_bit_list(binstring): 
     """Converts a string of '0's and '1's to a list of 0's and 1's"""
-    bitList = []
+    bit_list = []
     for bit in binstring:
-        bitList.append(int(bit))
-    return bitList
+        bit_list.append(int(bit))
+    return bit_list
 
-def padBits(bits, padding):
-    """Pad the input bits with 0's so that it is of length padding"""
-    return [0] * (padding-len(bits)) + bits
 
-def bitListToString(paddedBitSeq):
+def pad_bits(bit_list, desired_size):
+    """
+    Adds 0's to the head of bit_list so that bit_list has a length equal to desired_size. In other
+    words, adds [desired_size - len(bit_list)] 0's to the bit_
+
+    Args:
+        bit_list: a list of 0's and 1's
+        desired_size: an integer representing the desired size of the bit list
+
+    Returns:
+        The input bit_list with leading 0's inserted so that bit_list is of size desired_size.
+    """
+    if len(bit_list) > desired_size:
+        raise ValueError("Binary number is larger than desired size!")
+    return [0] * (desired_size-len(bit_list)) + bit_list
+
+
+def bit_list_to_string(paddedBitSeq):
     """Converts a list of 0's and 1's to a string of alpha/numeric characters"""
     charBuilder = ''
     # Iterate through by 8's becaause each padded bit sequence is 8 bits long
@@ -58,11 +103,11 @@ def intToString(integer):
     nteger of base 10 to a string based on the ASCII values of 
     its characters"""
     binary = bin(integer)[2:]
-    bitSeq = binstringToBitList(binary)
-    return bitListToString(padBits(bitSeq,(len(bitSeq)+(8-(len(bitSeq)%8)))))
+    bitSeq = binstring_to_bit_list(binary)
+    return bit_list_to_string(pad_bits(bitSeq,(len(bitSeq)+(8-(len(bitSeq)%8)))))
 
 ###### Example of converting a string to an int, and back to a string ######
 exampleString = "orange"
-exampleNum = stringToInt(exampleString)
+exampleNum = string_to_int(exampleString)
 backToString = intToString(exampleNum)
-print "{} {} {}\n".format(exampleString, exampleNum, backToString)
+print("{} {} {}".format(exampleString, exampleNum, backToString))
