b922492594d978f8c113bd3824b7be38fc2f4cd3
Ryan Tine <ryanptine@gmail.com>
2021-01-21 20:34:22 -0600

    Update docstrings. Refactor

--- string_to_int_to_string/string_to_int_to_string.py
+++ string_to_int_to_string/string_to_int_to_string.py
@@ -57,7 +57,16 @@ def string_to_int(string_to_convert):
 
 
 def binstring_to_bit_list(binstring): 
-    """Converts a string of '0's and '1's to a list of 0's and 1's"""
+    """
+    Converts a string composed of only 0's and 1's, into a list of 0's and 1's ()
+
+    Args:
+        binstring: a string containing only 0's and 1's
+
+    Returns:
+        A list of 0's and 1's, with identical frequency and ordering the 0's and 1's in the 
+        input string. 
+    """
     bit_list = []
     for bit in binstring:
         bit_list.append(int(bit))
@@ -75,28 +84,49 @@ def pad_bits(bit_list, desired_size):
 
     Returns:
         The input bit_list with leading 0's inserted so that bit_list is of size desired_size.
+    
+    Raises:
+        ValueError: The desired size of the padded list is smaller than the binary to be padded
     """
     if len(bit_list) > desired_size:
         raise ValueError("Binary number is larger than desired size!")
-    return [0] * (desired_size-len(bit_list)) + bit_list
+    num_zeros_needed = desired_size-len(bit_list)
+    padded_list = [0] * (num_zeros_needed) + bit_list
+    return padded_list
 
 
-def bit_list_to_string(paddedBitSeq):
+def bit_list_to_string(padded_bit_seq):
     """
     Converts a list of 0's and 1's to a string of alpha/numeric characters
-    """
-    charBuilder = ''
-    # Iterate through by 8's becaause each padded bit sequence is 8 bits long
-    for segment in range(0,len(paddedBitSeq),8): 
-        # concatenate each new char onto the built up string
-        charBuilder = charBuilder + bits_to_char(paddedBitSeq[segment: segment+8]) 
-    return charBuilder
 
-def bits_to_char(bitSeq):
-    """Converts each 8 bit length padded bit sequences 
-    back to a char based on its unicode value"""
+    Args:
+        padded_bit_seq: a list of 0's and 1's, such that the: (size of the list) mod 8 = 0
+
+    Returns:
+        A string of alpha/numeric characters
+    """
+    char_builder = ''
+    # Iterate through by 8's, as each padded bit sequence is 8 bits long
+    for segment in range(0,len(padded_bit_seq), 8): 
+        # concatenate each new char onto the built up string, following the pattern for encoding
+        char_builder = char_builder + bits_to_char(padded_bit_seq[segment: segment+8]) 
+    return char_builder
+
+def bits_to_char(bit_seq):
+    """
+    Converts a list of size 8, composed of 0's and 1's, into a character. Obtains the base 10 value
+    represented by the list of 0's and 1's, and finds the corresponding unicode char for that base
+    10 value.
+
+    Args:
+        bit_seq: a list of size 8, composed of 0's and 1's
+
+    Returns:
+        A character, whose unicode value is equal to the binary value encoded by the list of 0's 
+        and 1's
+    """
     value = 0
-    for bit in bitSeq:
+    for bit in bit_seq:
         value = (value * 2) + bit # This for loop will determine the numeric value of the binary bit sequence input
     return chr(value)
 
@@ -105,9 +135,13 @@ def int_to_string(integer_to_convert):
     Wrapper function for above four funtions to convert an integer of base 10 to a string based on the ASCII values of 
     its characters
     """
-    binary = bin(integer_to_convert)[2:]
-    bitSeq = binstring_to_bit_list(binary)
-    return bit_list_to_string(pad_bits(bitSeq,(len(bitSeq)+(8-(len(bitSeq)%8)))))
+    binary = bin(integer_to_convert)[2:] # convert input to string representing input's binary value
+    bit_seq = binstring_to_bit_list(binary)
+    bit_seq_size = len(bit_seq)
+    desired_bit_seq_size = bit_seq_size + (8-bit_seq_size % 8) # desired size is min size, such that size mod 8 = 0
+    padded_bit_seq = pad_bits(bit_seq, desired_bit_seq_size)
+    converted_string = bit_list_to_string(padded_bit_seq)
+    return converted_string
 
 ###### Example of converting a string to an int, and back to a string ######
 exampleString = "orange"
